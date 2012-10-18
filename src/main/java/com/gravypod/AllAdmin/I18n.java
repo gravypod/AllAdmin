c04b1b9f2d6a51c22eaca015af38ab9d25bf07af
Joshua D. Katz <gravypod@gravypod.com>
2012-10-18 18:01:50 -0400

    Update I18n

---
+++ src/main/java/com/gravypod/AllAdmin/I18n.java
@@ -0,0 +1,96 @@
+package com.gravypod.AllAdmin;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.bukkit.ChatColor;
+
+public class I18n {
+	
+	final String language = Locale.getDefault().getLanguage();
+	
+	final String country = Locale.getDefault().getCountry();
+	
+	final char colorCode = "&".toCharArray()[0];
+	
+	final Locale locale = new Locale(language, country);
+	
+	final ResourceBundle messages = ResourceBundle.getBundle("Messages", locale);
+	
+	final ResourceBundle defaultBundle = ResourceBundle.getBundle("Messages", Locale.ENGLISH);
+	
+	final ResourceBundle customMessages = ResourceBundle.getBundle("Messages", locale, new FileResClassLoader(I18n.class.getClassLoader(), AllAdmin.getInstance()));
+	
+	public String getColoredMessage(final String message) {
+	
+		try {
+			
+			try {
+				
+				return ChatColor.translateAlternateColorCodes(colorCode, customMessages.getString(message));
+				
+			} catch (MissingResourceException ex) {
+				
+				return ChatColor.translateAlternateColorCodes(colorCode, messages.getString(message));
+				
+			}
+			
+		} catch (MissingResourceException ex) {
+			return defaultBundle.getString(message);
+		}
+		
+	}
+	
+	/**
+	 * Taken from essentials
+	 */
+	private static class FileResClassLoader extends ClassLoader {
+		
+		private final transient File dataFolder;
+		
+		public FileResClassLoader(final ClassLoader classLoader, final AllAdmin allAdmin) {
+		
+			super(classLoader);
+			this.dataFolder = allAdmin.getDataFolder();
+			
+		}
+		
+		@Override
+		public URL getResource(final String string) {
+		
+			final File file = new File(dataFolder, string);
+			if (file.exists()) {
+				try {
+					return file.toURI().toURL();
+				} catch (MalformedURLException ex) {
+				}
+			}
+			return super.getResource(string);
+		}
+		
+		@Override
+		public InputStream getResourceAsStream(final String string) {
+		
+			final File file = new File(dataFolder, string);
+			
+			if (file.exists()) {
+				try {
+					return new FileInputStream(file);
+				} catch (FileNotFoundException ex) {
+				}
+			}
+			
+			return super.getResourceAsStream(string);
+			
+		}
+		
+	}
+	
+}
